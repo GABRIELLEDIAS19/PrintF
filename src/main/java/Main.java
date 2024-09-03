844697e6428ab3af307ab7cd32a60fbf6aafeb2d
Gabrielle Dias <169068323+GABRIELLEDIAS19@users.noreply.github.com>
2024-09-03 02:37:18 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+    String nome = "Gabrielle";
+    int dia = 19;
+    int mes = 01;
+    int ano = 2009;
+    double pi = 3.141592;
+    //System. out.printf("minha data de nascimento é: %d/%d/%d" , dia, mes, ano);
+    System.out.printf("o valor de pi é: %.2f" , pi);
+
+    
+    
+  }
+
+  
+}
